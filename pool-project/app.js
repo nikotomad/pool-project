32e8f23cf32835fa26e01402d5c2087d4cd5d57c
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-21 11:43:00 +0100

    tournament controllers + views + app.js

--- pool-project/app.js
+++ pool-project/app.js
@@ -60,7 +60,7 @@ app.use((req,res,next) => {
 
 app.use('/', index);
 app.use('/', authController);
-app.use('/tournament', tournamentController);
+app.use('/tournaments', tournamentController);
 // app.use('/center', centerController);
 // app.use('/user', userController);
 
--- pool-project/routes/tournamentController.js
+++ pool-project/routes/tournamentController.js
@@ -13,54 +13,31 @@ tournamentController.get("/show", (req, res, next) => {
   });
 });
 
-tournamentController.get("/:id", (req, res, next) => {
-  let id = req.params.id;
-
-  Tournament.findById(id, (err, tournament) => {
-    res.render('tournaments/detail', {
-      tournament: tournament
-    });
-  });
-});
+tournamentController.get('/:id', (req, res, next) => {
+  Tournament.findById(req.params.id)
+    .populate('creator')
+    .then(result => res.render('tournaments/detail', {
+      result
+    }));
+})
 
 tournamentController.get("/new", (req, res, next) => {
   res.render("tournaments/new");
 });
 
 tournamentController.post("/new", (req, res, next) => {
-  const name = req.body.name;
-  const level = req.body.level;
-  const maxParticipants = req.body.maxParticipants;
-  const startDate = req.body.startdate;
-  const endingDate = req.body.endingdate;
-
-  if (name === "" || level === "") {
-    res.render("tournaments/new", { message: "Indica un nombre de torneo y un nivel correspondiente." });
-    return;
-  }
-
-  Tournament.findOne({ name }, "name", (err, user) => {
-    if (name !== null) {
-      res.render("tournaments/new", { message: "El torneo ya existe, elige otro nombre" });
-      return;
-    }
-
-    const newTournament = new Tournament({
-      name,
-      level,
-      maxParticipants,
-      startDate,
-      endingDate
-    });
-
-    newTournament.save((err) => {
-      if (err) {
-        res.render("tournaments/new", { message: "Algo ha fallado" });
-      } else {
-        res.redirect("/");
-      }
-    });
+  const newTournament = new Tournament({
+    name: req.body.name,
+    level: req.body.level,
+    maxParticipants: req.body.maxParticipants,
+    startDate: req.body.startdate,
+    endingDate: req.body.endingdate,
+    creator: req.user._id
   });
+
+  newTournament.save()
+    .then(() => res.redirect("/"))
+    .catch(err => next(err))
 });
 
 module.exports = tournamentController;
