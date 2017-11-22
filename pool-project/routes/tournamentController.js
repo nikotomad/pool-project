82b73dd088c479fb17b3f2fa4aedeed69efce50a
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-22 18:05:33 +0100

    Ya te has dado de alta security works

--- pool-project/routes/tournamentController.js
+++ pool-project/routes/tournamentController.js
@@ -48,10 +48,17 @@ tournamentController.post("/new", ensureLogin.ensureLoggedIn('/login'), (req, re
 });
 
 tournamentController.get('/detail/:id', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
+  let isParticipant = false
   Tournament.findById(req.params.id)
    .populate("participants")
    .populate("creator")
-   .then(result =>  res.render("tournaments/detail",{ result }))
+   .then(result =>  {
+     for(let i = 0; i < result.participants.length; i++) {
+       if(result.participants[i].username === req.user.username)
+         isParticipant = true
+      }
+     res.render("tournaments/detail",{ result, isParticipant })
+   })
 });
 
 tournamentController.post('/detail/:id', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
