0a3d607e8e6af37752c1fbe7fa4624deaaea4926
Ernesmh <ernesmh@gmail.com>
2017-11-24 13:11:27 +0100

    putting promises

--- routes/userController.js
+++ routes/userController.js
@@ -22,37 +22,30 @@ userController.post("/", ensureLogin.ensureLoggedIn('/login'), (req, res, next) 
   const name = req.body.username;
   User.find({username: name})
     .then(users => res.render('user/show', {user: users}))
-    .catch(err => console.log("(eeeerrorrr"));
+    .catch(err => console.log("(Error"));
 });
 
 userController.get('/detail/:id', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
   const userId = req.params.id;
+  User.findById(userId)
+    .then(user => res.render('user/detail', { user: user }))
+    .catch(err => console.log("(Error"));
 
-  User.findById(userId, (err, user) => {
-    if (err) { return next(err); }
-    console.log(user);
-    res.render('user/detail', { user: user });
-
-  });
 });
+
 
 userController.get('/:id/edit', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
   const userId = req.params.id;
   Center.find({}, (err, center) => {
     if(err){ return next(err); }
-    console.log("kdkdkdkdk" + center);
   });
-  User.findById(userId, (err, user) => {
-    if (err) { return next(err); }
-    res.render('user/edit', {
-      user: user
-    });
-  });
+  User.findById(userId)
+    .then(user => res.render('user/edit', { user: user}))
+    .catch(err => console.log("Error"));
 });
 
 userController.post('/:id/edit', ensureLogin.ensureLoggedIn('/login'), upload.single('avatar'),(req, res, next) => {
   const userId = req.params.id;
-  console.log(req.body.avatar);
   const updates = {
       username: req.body.username,
       level: req.body.level,
@@ -63,10 +56,9 @@ userController.post('/:id/edit', ensureLogin.ensureLoggedIn('/login'), upload.si
       gamesWon: req.body.gamesWon
   };
 
-  User.findByIdAndUpdate(userId, updates, (err, user) => {
-    if (err){ return next(err); }
-    return res.redirect('/home');
-  });
+  User.findByIdAndUpdate(userId)
+    .then(user => res.redirect('/home'))
+    .catch(err => console.log("Error"));
 });
 
 
